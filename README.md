9ac8683ecb7e4218831e771fad5aa6cac8e22a8a
gregggilbert <gregg.gilbert@ivadolabs.com>
2022-12-25 16:45:31 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gregg's Mac M1 setup script
+Execute
+```
+./install.sh
+```
+
+then open iTerm2 and execute
+
+```
+conda init zsh
+```
+
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+xcode-select --install
+softwareupdate --install-rosetta
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+brew install miniforge
+
+# app with UI, see https://formulae.brew.sh/cask/
+brew install --cask google-chrome
+
+brew install --cask visual-studio-code
+brew install --cask pycharm
+brew install --cask postman
+brew install --cask docker
+
+brew install --cask messenger
+brew install --cask discord
+brew install --cask zoom
+brew install --cask slack
+
+brew install --cask spotify
+
+# terminal
+brew install --cask iterm2
+brew install --cask font-fira-code
+brew install starship
+echo "eval "$(starship init zsh)"" >>! ~/.zshrc
+starship preset plain-text-symbols > ~/.config/starship.toml
